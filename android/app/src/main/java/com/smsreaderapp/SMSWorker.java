df7721ba3c7143a84c9a9b762d6f3fef8d72eca5
Akash Yadav <109862673+akashyadavazm@users.noreply.github.com>
2024-07-29 08:52:11 +0530

    Update SMSWorker.java

--- android/app/src/main/java/com/smsreaderapp/SMSWorker.java
+++ android/app/src/main/java/com/smsreaderapp/SMSWorker.java
@@ -1,3 +1,21 @@
-public class SMSWorker {
+package com.smsreaderapp;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class SMSWorker extends Worker{
+    public SMSWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+      }
     
+      @NonNull
+      @Override
+      public Result doWork() {
+        // Define the work to be done here
+        return Result.success();
+      }
 }
+
